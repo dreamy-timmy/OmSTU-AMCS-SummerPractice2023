396a7eb1a8cbd214006f2a5f1830595b67f26248
tim <guntertimofej4@gmail.com>
2023-06-29 17:44:17 +0600

    UnitTestsAdded

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,15 +4,16 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = Math.Pow(10,-9);
+        double eps = 1e-8;
         if (Math.Abs(a) < eps) throw new System.ArgumentException();
         foreach(var x in new double[] {a,b,c})
         if (System.Double.IsNaN(x) || System.Double.IsInfinity(x)) throw new System.ArgumentException(); 
-
+        
         double D = Math.Pow(b,2) - 4*a*c;
-        if (Math.Abs(D) < eps ) return new double[] {-(b + Math.Sign(b)*Math.Sqrt(D))/(2*a)}; 
+        if (Math.Abs(D) < eps ) return new double[] {Math.Round(-(b + Math.Sign(b)*Math.Sqrt(D))/(2*a),4)}; 
         if (D <= -eps) return new double[0];
-        double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/(2*a), x2 = c/x1;
+        double x1 = Math.Round(-(b + Math.Sign(b)*Math.Sqrt(D))/(2*a),4), x2 = c/x1;
         return new double[] {x1,x2}; 
+        throw new NotImplementedException("Not fully implemented.");
     }
 }
